659c3d033f87bddf88b81d02914f3f29fe449bf2
Vignesh-R-R <91796541+Vignesh-R-R@users.noreply.github.com>
2021-12-17 17:45:51 +0530

    Update SyllabusProgram1.java

--- SyllabusProgram1.java
+++ SyllabusProgram1.java
@@ -8,7 +8,22 @@ twoVariable.number();
 twoVariable.decimalToBinary();
 twoVariable.binaryToDecimal();
 }
-
+public void secondLargestNumber(){
+	//find second largest number without using sorting
+	int arr[] = { 14, 46, 47, 86, 92, 52, 48, 36, 66, 87 };
+	int largest=arr[0];
+	int secondlargest=arr[0];
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]>largest) {
+			secondlargest=largest;
+			largest=arr[i];
+		}else if(arr[i]>secondlargest) {
+			secondlargest=arr[i];
+		}
+	}System.out.println("second largest number="+secondlargest);
+	}
+	
+	
 	private void binaryToDecimal() {
 	//using parseint
 	System.out.println(Integer.parseInt("101010", 2));
